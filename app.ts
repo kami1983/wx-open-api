a16c5d36d6b208426d3996082f28a31632aae2cc
kami1983 <kami@cancanyou.com>
2024-08-05 14:50:13 +0800

    Update user infos.

--- app.ts
+++ app.ts
@@ -81,6 +81,14 @@ app.post('/insertRentInfos', async (req, res) => {
 
 });
 
+// 获取 user infos
+app.get('/user/infos', async (req, res) => {
+    const open_id = req.headers['x-wx-openid'] as string ??''
+    // const open_id = 'o4IK35VLNtV7Cd_t0fiZKP67tOPU'
+    res.send({status: true, backData: {open_id}, headers: req.headers});
+    
+}
+
 app.get('/user/rent-infos', async (req, res) => {
     const { page = '1', limit = '10', type = '0'} = req.query; // 从请求中获取分页参数
     const open_id = req.headers['x-wx-openid'] as string ??''
